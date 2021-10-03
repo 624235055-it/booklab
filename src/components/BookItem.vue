d6c71522988d2d9cc77d197b9760aa715374d50b
Phongpol <624235055@parichat.skru.ac.th>
2021-10-03 20:31:50 +0700

    first commit

---
+++ src/components/BookItem.vue
@@ -0,0 +1,29 @@
+<template>
+        <div class ="book-item">
+            <h1>{{book.title}}</h1><br/>
+            <img v-bind:src="book.thumbnailUrl"/><br/>
+            <b>Author:</b>{{book.category}} <br/>
+            <b>Category:</b>{{book.author}} <br/>
+            <b>ISBN:</b>{{book.isbn}} <br/>
+            <b>Pages:</b>{{book.Pages}} <br/>
+            <b>Description:</b><br/>
+            <p>{{book.shortDescription}}</p>
+            
+        </div>
+
+</template>
+
+<script>
+export default {
+  name: 'BookItem',
+  components: {
+   
+  },
+  props:["book"]
+  
+  
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Books.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        
+        <div v-bind:key="book.id" v-for="book in books1">
+            <BookItem v-bind:book="book"/>
+        </div>
+
+
+    </div>
+</template>
+
+<script>
+import BookItem from './BookItem.vue'
+export default {
+  name: 'Books',
+  components: {
+   BookItem
+  },
+  props:["books1"]
+  
+}
+</script>
+
+<style>
+</style>
